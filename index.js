db7bdb1b2dfed6c83737fa713c50f8cb9d55104c
Cristiano Cruz <cristiano@linvix.com.br>
2023-07-18 20:35:15 -0300

    chore: add consts

--- index.js
+++ index.js
@@ -2,24 +2,50 @@ import { Dimensions, Platform, StatusBar, NativeModules } from "react-native";
 
 const { StatusBarManager } = NativeModules;
 
+const STATUSBAR_DEFAULT_HEIGHT = 20;
+const STATUSBAR_X_HEIGHT = 44;
+const STATUSBAR_IP12_HEIGHT = 47;
+const STATUSBAR_IP12MAX_HEIGHT = 47;
+const STATUSBAR_IP14PRO_HEIGHT = 49;
+
+// const X_WIDTH = 375;
+// const X_HEIGHT = 812;
+
+// const XSMAX_WIDTH = 414;
+// const XSMAX_HEIGHT = 896;
+
+// const IP12_WIDTH = 390;
+// const IP12_HEIGHT = 844;
+
+// const IP12MAX_WIDTH = 428;
+// const IP12MAX_HEIGHT = 926;
+
+// const IP14PRO_WIDTH = 393;
+// const IP14PRO_HEIGHT = 852;
+
+// const IP14PROMAX_WIDTH = 430;
+// const IP14PROMAX_HEIGHT = 932;
+
 export function isIphoneX() {
   const dimen = Dimensions.get("window");
 
+  const res_arr = [dimen.height, dimen.width];
+
   const resolutions =
     // i dont now
-    [dimen.height, dimen.width].includes(780) ||
+    res_arr.includes(780) ||
     // x, 11pro
-    [dimen.height, dimen.width].includes(812) ||
+    res_arr.includes(812) ||
     // 12, 13, 14, 13pro
-    [dimen.height, dimen.width].includes(844) ||
+    res_arr.includes(844) ||
     // 14 pro
-    [dimen.height, dimen.width].includes(852) ||
+    res_arr.includes(852) ||
     // 11, 11max
-    [dimen.height, dimen.width].includes(896) ||
+    res_arr.includes(896) ||
     // 12max, 13max
-    [dimen.height, dimen.width].includes(926) ||
+    res_arr.includes(926) ||
     // 14max
-    [dimen.height, dimen.width].includes(932);
+    res_arr.includes(932);
 
   return Platform.OS === "ios" && !Platform.isPad && !Platform.isTVOS && resolutions;
 }

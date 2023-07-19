930984d45bde49a10d34f3085ef75516f58a16a7
Cristiano Cruz <cristiano@linvix.com.br>
2023-07-19 11:01:00 -0300

    chore: fix type fallback to getBottomSpace

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,4 @@ export function isIphoneX(): boolean;
 export function ifIphoneX<T, U>(iphoneXVal: T, regularVal: U): T | U;
 export function ifIphoneX<T>(iphoneXVal: T): T;
 export function getStatusBarHeight(): number;
-export function getBottomSpace(fallback: number): number;
+export function getBottomSpace(fallback?: number): number;

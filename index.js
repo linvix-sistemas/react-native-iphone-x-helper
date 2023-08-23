3e9fd1ec44a5a00a5b45157794e4c8cd0fedfe11
Cristiano Cruz <cristiano@linvix.com.br>
2023-08-23 19:38:42 -0300

    chore: fix getStatusBarHeight

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ import { Dimensions, Platform, StatusBar, NativeModules } from "react-native";
 
 const { StatusBarManager } = NativeModules;
 
-const STATUSBAR_DEFAULT_HEIGHT = 20;
-const STATUSBAR_X_HEIGHT = 44;
-const STATUSBAR_IP12_HEIGHT = 47;
-const STATUSBAR_IP12MAX_HEIGHT = 47;
-const STATUSBAR_IP14PRO_HEIGHT = 49;
+const DEVICE_LAYOUT_MAX_VALUES = {
+  780: "iPhone", // i dont now
+  812: "iPhoneX", // x, 11pro
+  896: "iPhoneXMax", // 11, 11max
+  844: "iPhone12", // 12, 13, 14, 13pro
+  926: "iPhone12Max", // 12max, 13max
+  852: "iPhone14Pro", // 14 pro
+  932: "iPhone14ProMax", // 14max
+};
+
+const TARGET_IPHONE_OFFSET_HEIGHT = {
+  iPhone: 34,
+  iPhoneX: 44,
+  iPhoneXMax: 44,
+  iPhone12: 47,
+  iPhone12Max: 47,
+  iPhone14Pro: 59,
+  iPhone14ProMax: 59,
+};
 
 // const X_WIDTH = 375;
 // const X_HEIGHT = 812;
@@ -58,8 +72,15 @@ export function ifIphoneX(iphoneXStyle, regularStyle) {
 }
 
 export function getStatusBarHeight() {
+  const dimen = Dimensions.get("window");
+
+  const iPhoneTypeW = DEVICE_LAYOUT_MAX_VALUES?.[dimen.width] ?? null;
+  const iPhoneTypeH = DEVICE_LAYOUT_MAX_VALUES?.[dimen.height] ?? null;
+
+  const iPhoneHeight = TARGET_IPHONE_OFFSET_HEIGHT[iPhoneTypeW ?? iPhoneTypeH] ?? 34;
+
   return Platform.select({
-    ios: StatusBarManager.HEIGHT,
+    ios: iPhoneHeight,
     android: StatusBar.currentHeight,
     default: 0,
   });
